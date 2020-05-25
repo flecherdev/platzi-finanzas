4a6dd153c3c2a34a749b78039883315ae923f0cb
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-05-25 16:09:34 -0300

    se agrega recursos de imagenes y colores

---
+++ PlatziFinanzas/Application/Authentication/OnBoarding/OnBoardingConteinerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OnBoardingConteinerViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Ezequiel Freire on 25/05/2020.
+//  Copyright © 2020 Ezequiel Freire. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingConteinerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PlatziFinanzas/Extensions/Shadow.swift
@@ -0,0 +1,13 @@
+//
+//  Shadow.swift
+//  PlatziFinanzas
+//
+//  Created by Ezequiel Freire on 25/05/2020.
+//  Copyright © 2020 Ezequiel Freire. All rights reserved.
+//
+
+import UIKit
+
+class Shadow: NSObject {
+
+}
