4834c4a818665b2a01e2f56e8ced6b4cd17c9732
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-05-31 18:06:52 -0300

    se agrega onboardin steps, transacciones e imagenes

---
+++ PlatziFinanzas/Application/Authentication/OnBoarding/OnBoardingStepsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  OnBordingStepsViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Ezequiel Freire on 30/05/2020.
+//  Copyright © 2020 Ezequiel Freire. All rights reserved.
+//
+
+import UIKit
+
+class OnBordingStepsViewController: UIViewController {
+    // referencias a nuestra estructura de datos (1)
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var OnBoardingImageLabel  : UIImageView!
+
+    var item : OnBordingItem? //? el signo determina si es opcional (2)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // asignamos la informacion de item a nuestro label (3)
+        titleLabel.text = item?.title
+        descriptionLabel.text = item?.description
+        OnBoardingImageLabel.image = UIImage(named: item?.imageName ?? "") // como el valor es opcional y el UIImage necesita uno
+    }
+
+}
+
+
